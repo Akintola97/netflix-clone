7952c098c69120b7924f87a9ca5e8e709a931cec
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-03-06 17:30:25 -0600

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
 
   return (
     <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
-    <Link to = '/'>
+    <Link to = '/netflix'>
         <h2 className='text-red-600 text-4xl font-bold cursor-pointer'>
             NETFLIX
         </h2>
